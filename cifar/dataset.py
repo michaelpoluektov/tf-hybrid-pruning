a79489ad2da322a63723c4c073a128ce03973118
Michael Poluektov <michael.poluektov@gmail.com>
2023-02-12 19:22:01 +0000

    binary search activation constant-ifier

--- cifar/dataset.py
+++ cifar/dataset.py
@@ -6,15 +6,7 @@ y_train = tf.keras.utils.to_categorical(y_train, 100)
 y_test = tf.keras.utils.to_categorical(y_test, 100)
 
 
-def get_dataset(is_train, size, epochs, batch_size):
-    data_augmentation = tf.keras.Sequential(
-        [
-            tf.keras.layers.RandomFlip("horizontal"),
-            tf.keras.layers.RandomRotation(0.2),
-        ]
-    )
-
-    xy = (x_train if is_train else x_test, y_train if is_train else y_test)
+def _get_dataset(is_train, size, epochs, batch_size, xy):
     return (
         tf.data.Dataset.from_tensor_slices(xy)
         .batch(batch_size)
@@ -23,3 +15,22 @@ def get_dataset(is_train, size, epochs, batch_size):
         .prefetch(tf.data.AUTOTUNE)
         .repeat(epochs)
     )
+
+
+def get_dataset(is_train, size, epochs, batch_size, split=False):
+    data_augmentation = tf.keras.Sequential(
+        [
+            tf.keras.layers.RandomFlip("horizontal"),
+            tf.keras.layers.RandomRotation(0.2),
+        ]
+    )
+
+    xy = (x_train if is_train else x_test, y_train if is_train else y_test)
+    if split:
+        half = len(xy[0]) // 2
+        test_xy = (xy[0][:half], xy[1][:half])
+        val_xy = (xy[0][half:], xy[1][half:])
+        test_ds = _get_dataset(is_train, size, epochs, batch_size, test_xy)
+        val_ds = _get_dataset(is_train, size, epochs, batch_size, val_xy)
+        return (test_ds, val_ds)
+    return _get_dataset(is_train, size, epochs, batch_size, xy)
