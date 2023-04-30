77614c09851c6bd99fd01dff708cddc28b40a921
Michael <michael.poluektov@gmail.com>
2023-04-30 18:12:49 +0100

    refactor start

--- README.md
+++ README.md
@@ -1,18 +1,24 @@
-# sparse-riscv
-TB2 individual project -- Sparsifying CNN's and optimising for RISC-V
+# tf-hybrid-pruning
+
+TB2 individual project -- exploring hybrid optimisation methods for data-free inference optimisations on mobile devices
 
 ## Motivation
-This project aims to create a wrapper that, given a pre-trained deep CNN will optimise it for inference on RISC-V processors. We assume that we only have access to the test/validation data.
-As of now no part of this project is specific to RISC-V, but since RISC-V is commonly used for low-power devices this is what we will later use to implement TFLite operators and benchmark our model.
+
+The goal of this project is to create a data-free technique to compress Convolutional Neural Networks (CNN) for inference on mobile hardware. The intended use case is to support optimisations for a wide variety of hardware architectures and resource constraints.
+This project would enable developers to train a single network then optimise it for deployment for arbitrary devices such as embedded systems, mobile phones or web applications.
 
 ## Approach
+
 There are 3 steps to this project:
+
 1. Reducing the complexity of our model
 2. Implementing TFLite operators that can take advantage of that reduced complexity
 3. Tweaking step 1 to fit step 2 better and vice-versa
 
 ### Reducing complexity
+
 So far the following ways of reducing model complexity are being considered:
+
 - Weight pruning based on magnitude
 - Activation pruning based on magnitude
 - Re-structuring the model under the assumptions that certain activations can be treated as constants
@@ -23,17 +29,21 @@ The 3rd point can be done using binary search, pruning the activations with the 
 The 4th point can be done by finding highly correlated activations, but that may be too slow.
 
 ### Implementing TFLite operators
+
 This will most likely involve implementing a sparse Conv2D layer with activation pruning. We assume the possibility to use P and V extensions.
 
 ### Tweaking
+
 Can we co-design the pruning method and the operator?
 
 ## File structure
 
 >
+
     .
     ├── setup.txt                    # Very non-descriptive setup guide
     ├── cifar/                       # Pruning on a CIFAR model
     ├── mnist_test/                  # Used to test if the installation
     └── README.md
+
 >
---
+++ src/model.py
@@ -0,0 +1,103 @@
+import tensorflow as tf
+import numpy as np
+from utils import get_decomp
+
+# from sparse_conv2d import SparseConv2D, clone_function
+from custom_resnet import ResNet50
+
+
+# copied from Kaggle notebook, will cite in write-up
+def get_resnet():
+    base_model = tf.keras.applications.resnet.ResNet50(
+        include_top=False, weights="imagenet", input_shape=(224, 224, 3)
+    )
+    # base_model = tf.keras.models.clone_model(base_model, clone_function=clone_function)
+    model = tf.keras.models.Sequential()
+    model.add(tf.keras.layers.UpSampling2D(size=(7, 7), interpolation="bilinear"))
+    model.add(base_model)
+    model.add(tf.keras.layers.GlobalAveragePooling2D())
+    model.add(tf.keras.layers.Dropout(0.25))
+    model.add(tf.keras.layers.Dense(256, activation="relu"))
+    model.add(tf.keras.layers.BatchNormalization())
+    model.add(tf.keras.layers.Dense(100, activation="softmax"))
+    ins = np.zeros(shape=(1, 32, 32, 3))
+    _ = model(ins)
+    return base_model, model
+
+
+def get_resnet_scratch():
+    base_model, model = get_resnet()
+    model.load_weights("model/resnet_l1.h5")
+    return base_model, model
+
+
+def get_imagenet_resnet():
+    base_model, model = get_resnet()
+    base_model.trainable = False
+    # train yourself or load weights
+    return base_model, model
+
+
+def _decomp_resnet(ranks):
+    ins = tf.keras.layers.Input(shape=(32, 32, 3))
+    x = tf.keras.layers.UpSampling2D(size=(7, 7), interpolation="bilinear")(ins)
+    x = ResNet50(ranks=ranks, include_top=False, weights=None, input_tensor=x)
+    x = tf.keras.layers.GlobalAveragePooling2D()(x)
+    x = tf.keras.layers.Dropout(0.25)(x)
+    x = tf.keras.layers.Dense(256, activation="relu")(x)
+    x = tf.keras.layers.BatchNormalization()(x)
+    x = tf.keras.layers.Dense(100, activation="softmax")(x)
+    model = tf.keras.Model(inputs=ins, outputs=x)
+    return model
+
+
+def get_decomp_resnet(ranks, spars):
+    model = _decomp_resnet(ranks)
+    name_set = set([l.name for l in model.layers])
+    base_model, model2 = get_resnet_scratch()
+    bias3 = [
+        l
+        for l in base_model.layers
+        if isinstance(l, tf.keras.layers.Conv2D) and l.kernel.shape[0] == 3
+    ]
+    for i, l in enumerate(bias3):
+        rank = ranks[i]
+        if rank == 0:
+            model.get_layer(l.name).set_weights(l.get_weights())
+            name_set.remove(l.name)
+            continue
+        spar = spars[i]
+        core, first, last, sp = get_decomp(
+            l.kernel.numpy(), (2, 3), [rank, rank], 100 - spar
+        )
+        model.get_layer(l.name + "_first").set_weights([np.expand_dims(first, [0, 1])])
+        model.get_layer(l.name + "_core").set_weights([core])
+        model.get_layer(l.name + "_last").set_weights([np.expand_dims(last.T, [0, 1])])
+        model.get_layer(l.name + "_sp").set_weights([sp, l.bias.numpy()])
+        name_set.remove(l.name + "_sp")
+        name_set.remove(l.name + "_first")
+        name_set.remove(l.name + "_core")
+        name_set.remove(l.name + "_last")
+
+    for l in base_model.layers:
+        if l not in bias3 and "input" not in l.name:
+            model.get_layer(l.name).set_weights(l.get_weights())
+            name_set.remove(l.name)
+    model2_names = ["dense_2", "batch_normalization_1", "dense_3"]
+    model_names = ["dense", "batch_normalization", "dense_1"]
+    for m2, m in zip(model2_names, model_names):
+        model.get_layer(m).set_weights(model2.get_layer(m2).get_weights())
+    return model
+    # model.load_weights("model/custom_resnet50.h5", by_name=True)
+    # return model
+
+
+if __name__ == "__main__":
+    import pickle
+
+    with open("model/resnet_bn_finetune_compress_and_val.pickle", "rb") as f:
+        props = pickle.load(f)
+        decomps = [p[0] for p in props]
+        spars = [p[1] for p in props]
+        model = get_decomp_resnet(decomps, spars)
+        model.save("model/resnet_bn_finetune_decomped.h5")
